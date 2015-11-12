092b4b0fb6eeaf59a059e57de039e5779c0b23e2
benh <splodgemolly@gmail.com>
2015-11-12 09:25:57 +0000

    Implemented tick for mob class

--- MCsim/src/main/Mob.java
+++ MCsim/src/main/Mob.java
@@ -10,53 +10,94 @@ public abstract class Mob {
 	protected DIRECTION MOB_DIRECTION;
 	protected BufferedImage asset;
 	protected BufferedImage possibleAssets[];
-	
+	protected int speedMultiplier;
+
 	public Mob next;
-	
-	public Mob(int spawnPosX, int spawnPosY, String givenAssetName) {
-	
+
+	public Mob(int spawnPosX, int spawnPosY, String givenAssetName, int givenSpeedMultipler) {
+
 		x = spawnPosX;
 		y = spawnPosY;
-		
+
 		MOB_DIRECTION = DIRECTION.STILL;
-		
-		
-		
+
+		speedMultiplier = givenSpeedMultipler;
+
 	}
-	
+
 	public Graphics2D render(Graphics2D g2d) {
+
+		getInputs();
 		
 		switch(MOB_DIRECTION){
 		case EAST:
+			x = x + speedMultiplier;
 			break;
 		case NORTH:
+			y = y - speedMultiplier;
 			break;
 		case NORTH_EAST:
+			y = y - speedMultiplier;
+			x = x + speedMultiplier;
 			break;
 		case NORTH_WEST:
+			x = x - speedMultiplier;
+			y = y - speedMultiplier;
 			break;
 		case SOUTH:
+			y = y - speedMultiplier;
 			break;
 		case SOUTH_EAST:
+			x = x + speedMultiplier;
+			y = y - speedMultiplier;
 			break;
 		case SOUTH_WEST:
+			y = y - speedMultiplier;
+			x = x - speedMultiplier;
 			break;
 		case STILL:
 			break;
 		case WEST:
+			x = x - speedMultiplier;
 			break;
 		default:
 			break;
-		
+
 		}
-		
+
 		return g2d;
-		
+
 	}
-	
+
 	public void tick() {
-		
+
+		if(x > 0 && x < 1000 && y > 0 && y < 500) {
+			switch(MOB_DIRECTION) {
+			case EAST:
+				break;
+			case NORTH:
+				break;
+			case NORTH_EAST:
+				break;
+			case NORTH_WEST:
+				break;
+			case SOUTH:
+				break;
+			case SOUTH_EAST:
+				break;
+			case SOUTH_WEST:
+				break;
+			case STILL:
+				break;
+			case WEST:
+				break;
+			default:
+				break;
+
+			}
+		}
+
 	}
-	
+
 	public abstract void getInputs();	
 }

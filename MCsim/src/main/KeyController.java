61fa4f9613ab1f247146c2887ae61c29bb8d7acd
benh <splodgemolly@gmail.com>
2015-11-16 12:24:41 +0000

    Added Key Controller

---
+++ MCsim/src/main/KeyController.java
@@ -0,0 +1,29 @@
+package main;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyController implements KeyListener{
+
+	public KeyController() {
+		
+	}
+
+	public void keyPressed(KeyEvent arg0) {
+
+		
+	}
+
+
+	public void keyReleased(KeyEvent arg0) {
+
+		
+	}
+
+
+	public void keyTyped(KeyEvent arg0) {
+
+		
+	}
+	
+}

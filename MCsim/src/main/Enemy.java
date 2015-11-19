7990fc7a8b40a158349270dfcac2efe73240a1b0
benh <splodgemolly@gmail.com>
2015-11-19 09:12:43 +0000

    completed enemy getInputs function

--- MCsim/src/main/Enemy.java
+++ MCsim/src/main/Enemy.java
@@ -2,18 +2,61 @@ package main;
 
 public class Enemy extends Mob{
 
-	public Enemy(int spawnPosX, int spawnPosY) {
-		
+	private Player mainPlayer;
+
+	private int instructionLifeTime;
+
+	public Enemy(int spawnPosX, int spawnPosY, Player givenPlayer) {
+
 		super(spawnPosX, spawnPosY, "enemy");
-		
+
 		speedMultiplier = 2;
-		
+
+		mainPlayer = givenPlayer;
+
+		instructionLifeTime = 0;
+
 	}
 
 	public void getInputs() {
+
+		int deltaX = x - mainPlayer.x;
+		int deltaY = y - mainPlayer.y;
+
+
+		if(deltaX > -10 && deltaX < 10 && deltaY > -10 && deltaX < 10) {//If the enemy is within 10 units of the mob, it should destroy itself;
+			if(next != null) {next.prev = prev;}
+			prev.next = next;
+		} else if (instructionLifeTime == 0){//Work out pathing if an instruction has not been issued recently;
+
+			if(deltaY < 0) {
+				if(deltaX > 100) {
+					MOB_DIRECTION = DIRECTION.NORTH_EAST;
+				}else if(deltaX < -100) {
+					MOB_DIRECTION = DIRECTION.NORTH_WEST;
+				} else {
+					MOB_DIRECTION = DIRECTION.NORTH;
+				}
+			} else if(deltaY > 0) {
+				if(deltaX > 100) {
+					MOB_DIRECTION = DIRECTION.SOUTH_EAST;
+				} else if(deltaX < -100) {
+					MOB_DIRECTION = DIRECTION.SOUTH_WEST;
+				} else {
+					MOB_DIRECTION = DIRECTION.SOUTH;
+				}
+			}
+
+			instructionLifeTime = 60;
+
+		} else {
+
+			instructionLifeTime--;
+
+		}
 		
-		
-		
+		setTextBox();
+
 	}
 
 }

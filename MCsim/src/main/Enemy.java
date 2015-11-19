d563b096717b7b032955633e45551a31a853943a
benh <splodgemolly@gmail.com>
2015-11-19 09:13:53 +0000

    fixed comments

--- MCsim/src/main/Enemy.java
+++ MCsim/src/main/Enemy.java
@@ -27,7 +27,7 @@ public class Enemy extends Mob{
 		if(deltaX > -10 && deltaX < 10 && deltaY > -10 && deltaX < 10) {//If the enemy is within 10 units of the mob, it should destroy itself;
 			if(next != null) {next.prev = prev;}
 			prev.next = next;
-		} else if (instructionLifeTime == 0){//Work out pathing if an instruction has not been issued recently;
+		} else if (instructionLifeTime == 0){//Work out direction if an instruction has not been issued recently;
 
 			if(deltaY < 0) {
 				if(deltaX > 100) {

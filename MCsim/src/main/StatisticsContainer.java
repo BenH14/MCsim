58f03d45f378f689b5e9ae1ac705b749eb0a3b26
benh <splodgemolly@gmail.com>
2015-12-04 14:25:12 +0000

    implemented timings into stat container

--- MCsim/src/main/StatisticsContainer.java
+++ MCsim/src/main/StatisticsContainer.java
@@ -2,4 +2,24 @@ package main;
 
 public class StatisticsContainer {
 
+	//Time
+	public int gameTime; //In ticks
+	public double gameTimeSecs; //In seconds
+	
+	public StatisticsContainer() {
+
+		gameTime = 0;
+		gameTimeSecs = 0;
+		
+	}
+	
+	public void tick() {
+		
+		//Update Time
+		gameTime++;
+		gameTimeSecs = gameTimeSecs / 60;
+		
+		
+	}
+	
 }

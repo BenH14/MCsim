af9d2ada68e729943b55ff3298cc17698bb1fafb
benh <splodgemolly@gmail.com>
2015-12-04 09:01:01 +0000

    Created effect abstract class

--- MCsim/src/effects/Effect.java
+++ MCsim/src/effects/Effect.java
@@ -1,12 +1,15 @@
 package effects;
 
-public class Effect {
+import java.awt.Graphics2D;
+
+public abstract class Effect {
 
 	//Linked List
 	public Effect next;
 	
-	public Effect() {
-		
-	}
+	public int lifeTime;
+	
+	public abstract void tick();
+	public abstract Graphics2D render(Graphics2D g2d);
 	
 }

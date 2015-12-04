1b7d6b5f9e295922851b2377a0e09011d74b1eb0
benh <splodgemolly@gmail.com>
2015-12-04 09:16:30 +0000

    Added screenshake class

---
+++ MCsim/src/effects/ScreenShake.java
@@ -0,0 +1,18 @@
+package effects;
+
+import java.awt.image.BufferedImage;
+
+public class ScreenShake extends Effect{
+
+
+	public void tick() {
+
+
+	}
+
+	public BufferedImage render(BufferedImage g2d) {
+
+		return g2d;
+	}
+
+}

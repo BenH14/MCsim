38b04f39d203114f0170e4625a4dfe5a2f57a418
benh <splodgemolly@gmail.com>
2016-01-13 10:21:00 +0000

    Made stop loop work

--- MCsim/src/main/SoundManager.java
+++ MCsim/src/main/SoundManager.java
@@ -16,43 +16,45 @@ public class SoundManager extends Thread{
 	private Pulse pulser;
 
 	public SoundManager() {
-//
-//		stop = false;
-//
-//		players = new Clip[10];
-//		
-//		try {
-//			//Open Files
-////			drums = AudioSystem.getAudioInputStream(new File("/res/music/drums.mp3"));
-//		} catch (Exception ex) {
-//			ex.printStackTrace();
-//		}
+		//
+		//		stop = false;
+		//
+		//		players = new Clip[10];
+		//		
+		//		try {
+		//			//Open Files
+		////			drums = AudioSystem.getAudioInputStream(new File("/res/music/drums.mp3"));
+		//		} catch (Exception ex) {
+		//			ex.printStackTrace();
+		//		}
 	}
 
 	public void stopLoop() {
-//		for (int x = 0;x < 10; x++) {
-//			if(players[x].isRunning()) {
-//				players[x].stop();	
-//			}
-//		}
+		for (int x = 0;x < 10; x++) {
+			if(players[x] != null) {
+				if(players[x].isRunning()) {
+					players[x].stop();	
+				}
+			}
+		}
 	}
 
 	public void run() {
 
 		stop = false;
-		
+
 		pulser = new Pulse(140);
 		EffectManager.addEffect(pulser);
-//		try {
-//			players[0] = AudioSystem.getClip();
-//			players[0].open(drums);
-//		} catch (LineUnavailableException | IOException e) {
-//			e.printStackTrace();
-//		}
-//
-//		players[0].setFramePosition(0);
-//		players[0].start();
-//		players[0].loop(Clip.LOOP_CONTINUOUSLY);
+		//		try {
+		//			players[0] = AudioSystem.getClip();
+		//			players[0].open(drums);
+		//		} catch (LineUnavailableException | IOException e) {
+		//			e.printStackTrace();
+		//		}
+		//
+		//		players[0].setFramePosition(0);
+		//		players[0].start();
+		//		players[0].loop(Clip.LOOP_CONTINUOUSLY);
 
 
 

3ee50b84c6ef4c76ce6050a38434db1d3d65bc03
benh <splodgemolly@gmail.com>
2016-01-28 13:54:31 +0000

    Updated logging utilities to include startup

--- MCsim/src/logging/FileLogger.java
+++ MCsim/src/logging/FileLogger.java
@@ -3,33 +3,38 @@ package logging;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class FileLogger extends Logger{
-	
+
+	private File f;
+
 	public FileLogger(URGENCY GIVEN_URGENCY) throws FileNotFoundException {
-		
+
 		super(GIVEN_URGENCY);
-		
-		File f = new File("log.txt");
-		if(!f.exists()) { 
-		   throw new FileNotFoundException();
+
+		f = new File("log.txt");
+		if(!f.exists()) {
+				throw new FileNotFoundException();
 		}
-		
+
 	}
-	
+
 	protected void print(String message) {
-		
+
 		try {
-			PrintWriter out = new PrintWriter(new FileOutputStream(new File("log.txt"),true));
+			PrintWriter out = new PrintWriter(new FileOutputStream(f,true));
 			out.println(message );
 			out.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 
-		
+
 	}
-	
+
 
 }
--- MCsim/src/logging/Logger.java
+++ MCsim/src/logging/Logger.java
@@ -1,5 +1,9 @@
 package logging;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
@@ -12,40 +16,63 @@ public abstract class Logger {
 		FATAL,
 		UNKOWN
 	}
-	
+
 	protected URGENCY LOG_URGENCY;
-	
+
 	public Logger() {
 		LOG_URGENCY = URGENCY.UNKOWN;
 	}
-	
+
 	public Logger(URGENCY GIVEN_URGENCY) {
 		LOG_URGENCY = GIVEN_URGENCY;
 	}
-	
+
 	public void write(String message) {
-		
+
 		Calendar cal = Calendar.getInstance();
-        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-		
+		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+
 		switch(LOG_URGENCY) {
 		case DEBUG: message = "<#>   - " + sdf.format(cal.getTime()) + " : " + message;
-			break;
+		break;
 		case ERROR: message = "<!>   - " + sdf.format(cal.getTime()) + " : " + message;
-			break;
+		break;
 		case FATAL: message = "<!!!> - " + sdf.format(cal.getTime()) + " : " + message;
-			break;
+		break;
 		case STATUS:message = "<OK>  - " + sdf.format(cal.getTime()) + " : " + message;
-			break;
+		break;
 		case UNKOWN:message = "<?>   - " + sdf.format(cal.getTime()) + " : " + message;
-			break;
+		break;
 		}
-		
+
 		print(message);
 	}
-	
+
+	public void startup() {
+
+		File f = new File("log.txt");
+
+		try {
+			
+			f.createNewFile();
+
+			PrintWriter out = new PrintWriter(new FileOutputStream(f,false));
+
+			Calendar cal = Calendar.getInstance();
+			SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+
+			out.println("New Launch at " + sdf.format(cal.getTime()));
+
+			out.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
 	protected abstract void print(String message);
-	
-	
-	
+
+
+
 }

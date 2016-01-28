c1217045151736d2dc5236cc4b6b937843f3e0f9
benh <splodgemolly@gmail.com>
2016-01-28 14:00:38 +0000

    Added logging support for launch

--- MCsim/src/main/Launch.java
+++ MCsim/src/main/Launch.java
@@ -1,11 +1,21 @@
 package main;
 
+import logging.DebugFactory;
+import logging.Logger;
+
 public class Launch {
 
 	public static void main(String[] args) {
 		
+		DebugFactory.getDebug(Logger.URGENCY.STATUS).startup();
+		
 		SuperController sc = new SuperController();
+		
+		DebugFactory.getDebug(Logger.URGENCY.STATUS).write("Starting Main Controller Loop");
+		
 		sc.loop();
+		
+		DebugFactory.getDebug(Logger.URGENCY.FATAL).write("Main Controller Loop has ended, program ending");
 
 	}
 

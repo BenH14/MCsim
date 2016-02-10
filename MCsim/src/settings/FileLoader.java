3c39d75799238354611c47ece38dd5901b0a1c0f
benh <splodgemolly@gmail.com>
2016-02-10 10:56:26 +0000

    Added support for both input and output streams in fileloader

--- MCsim/src/settings/FileLoader.java
+++ MCsim/src/settings/FileLoader.java
@@ -1,12 +1,24 @@
 package settings;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 //This just gets around the fact that settingsmanager is static so i can load the file
 public class FileLoader {
 
-	public InputStream getStream(String path) {
+	public InputStream getInStream(String path) {
 		return getClass().getClassLoader().getResourceAsStream(path);
+	}
+	
+	public OutputStream getOutStream(String path) throws FileNotFoundException, URISyntaxException {
+		URL resourceUrl = getClass().getClassLoader().getResource(path);
+		File file = new File(resourceUrl.toURI());
+		return new FileOutputStream(file);
 	}
 	
 }

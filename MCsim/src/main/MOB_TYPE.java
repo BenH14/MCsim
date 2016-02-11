93f1d9bb11c00c1bdc79d68755c8fba6b00354b5
benh <splodgemolly@gmail.com>
2016-02-11 09:07:08 +0000

    Fixed phrases for player, added bias

--- MCsim/src/main/MOB_TYPE.java
+++ MCsim/src/main/MOB_TYPE.java
@@ -26,17 +26,18 @@ public enum MOB_TYPE {
 
 	ENEMY(new String[]{
 			//PHRASES
-			"Wheres your coursework?",
-			"Jamie that breaks the chairs, yeah?",
-			"Ben, I don't want to hear any more from you",
-			"Need it by the end of the week, monday at the latest",
-			"We don't need a running commentary",
-			"Lets not waste any more time, yeah?",
-			"Ahad, wheres your extended essay?",
-			"Correct format is 120% of the marks",
-			"Hindolo, this isn't a maths lesson",
-			"Just do it",
-			"Google it, yeah?"
+			"I just need to print it off",
+			"Its on my computer at home",
+			"My printer is broken",
+			"The program is done, I just need to do the documentation",
+			"I am doing it right now",
+			"I have it on my memory stick",
+			"I forgot to send it",
+			"I'll email it tonight",
+			"I thought you said monday",
+			"Did you not get my email?",
+			"I can't get eclipse to work",
+			null,null,null,null,null,null,null,null,null,null //Bias towards collins
 	},
 			new String[]{
 					//NAMES
